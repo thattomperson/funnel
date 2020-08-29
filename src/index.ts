e05c01ab3f7a8d7629e58ddf80791c52ffcdb22b
Tom Albrighton <thomas.albrighton@four.io>
2020-08-29 10:37:02 +0930

    Added pusher js client

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-
+import Pusher from 'pusher-js';
 
 // Enable pusher logging - don't include this in production
 Pusher.logToConsole = true;
 
-var pusher = new Pusher(process.env.PUSHER_KEY, { cluster: 'ap4' });
+var pusher = new Pusher(process.env.PUSHER_KEY, { cluster: process.env.PUSHER_CLUSTER });
 
 var channel_name = `channel-${window.location.hash.substr(1)}`;
 document.getElementById('channel').innerText = channel_name
